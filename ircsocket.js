87efa9f75137172d3df5c018e73b0f9fa571226d
Tom <thr2@aber.ac.uk>
2014-12-24 17:48:28 +0000

    added tls support
    closes issue 1

--- ircsocket.js
+++ ircsocket.js
@@ -1,7 +1,9 @@
 var net     = require('net');
+var tls     = require('tls');
+var fs      = require('fs');
 var colours = require('colors')
 
-var socket = null;
+var socket  = null;
 
 var cTimeout = 600000;
 
@@ -9,12 +11,35 @@ var gPort                = 0;
 var gUrl                 = '';
 var gOnDataCallBack      = null;
 var gOnReconnectCallBack = null;
+var gTLS                 = false;
 
-function connect(url, port, onDataCallBack, onReconnectCallBack){
+var options = {};
+
+function connect(url, port, tls, onDataCallBack, onReconnectCallBack){
 	gUrl                 = url;
 	gPort                = port;
+	gTLS                 = tls;
 	gOnDataCallBack      = onDataCallBack;
 	gOnReconnectCallBack = onReconnectCallBack;
+
+	if (tls) {
+
+		var priKeyPath = 'private-key.pem';
+		var certPath   = 'public-cert.pem';
+
+		var fKey  = fs.existsSync(priKeyPath) ? fs.readFileSync(priKeyPath) : null;
+		var fCert = fs.existsSync(certPath)   ? fs.readFileSync(certPath)   : null;
+
+		if (!fKey || !fCert) {
+			console.error('TLS Cert or Private Key missing!'.red);
+			process.exit(1);
+		}
+
+		options = {
+			key:  fKey,
+			cert: fCert
+		};
+	}
 
 	inConnect();
 }
@@ -22,7 +47,12 @@ function connect(url, port, onDataCallBack, onReconnectCallBack){
 function inConnect() {
 
 	if (!socket) {
-		socket = net.Socket();
+		if (tls) {
+			socket = tls.connect(gPort, gUrl, options);
+		} else {
+			socket = net.Socket();
+			socket.connect(gPort, gUrl);
+		}
 
 		socket.setTimeout(cTimeout);
 
@@ -48,7 +78,6 @@ function inConnect() {
 			}
 		);
 
-		socket.connect(gPort, gUrl);
 	} else {
 		console.error('Socket already open!');
 	}
@@ -67,7 +96,7 @@ function handleReconnect() {
 
 function sendData(data) {
 	if (socket) {
-		console.log(('<- ' + String(data)).red);
+		console.log(('<- ' + String(data)).yellow);
 		socket.write(data + '\r\n');
 	} else {
 		console.error('There is no socket open!');
--- server.js
+++ server.js
@@ -3,9 +3,10 @@ var irc = require('./irc');
 console.log('Starting application');
 
 irc.connect(
-	'irc.aberwiki.org',
-	6667, 
+	'holmes.freenode.net',
+	7070, 
 	'SpunkyJr',
+	true,
 	function(data) {
 		//console.log('some new data ' + data);
 	},
@@ -18,8 +19,8 @@ joinServer();
 
 function joinServer() {
 	irc.joinChannel('#xetk');
-//	irc.sendPrivMsg('#xetk',   'Hello World');
-//	irc.sendActionMsg('#xetk', 'Hello World'); 
+	irc.sendPrivMsg('#xetk',   'Hello World');
+	irc.sendActionMsg('#xetk', 'Hello World'); 
 }
 
 console.log('Ending application');
